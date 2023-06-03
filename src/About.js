f547a4df9b31d1b0d84521b0c96d8522720caf1b
windows <windows@microsoft.com>
2023-06-03 12:11:04 +0530

    change

---
+++ src/About.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+function Myabout()
+{
+    return "this is return result";  
+}
+
+export default Myabout
+
+
+
+export function Myabout1()
+{
+    return <h1>lsdjfkljsdklfjksdjf </h1>;
+}
